06a544dc9499f81c918b0245cc4d867cc912be46
Dan Machinez <machinezdanilo@gmail.com>
2021-07-21 17:23:53 -0300

    validando o formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,52 @@
 let inputNome = document.querySelector('#nome')
 let inputEmail = document.querySelector('#email')
-let texteareaMensagem = document.querySelector('#mensagem')
+let inputTxtArea = document.querySelector('#mensagem')
 let btnEnviar = document.querySelector('#enviar')
+let nomeLiberado = false
+let emailLiberado = false
+let txtAreaLiberado = false
 
 
 inputNome.addEventListener('keyup', () => {
-    if(inputNome.value.length < 2){
+    if(inputNome.value == '""' ||inputNome.value == null || inputNome.value.length < 3){
         inputNome.style.borderColor = 'red'
+        nomeLiberado = false
     } else {
         inputNome.style.borderColor = 'green'
+        nomeLiberado = true
     }
 })
 
 inputEmail.addEventListener('keyup', () => {
-    if(inputEmail.value.indexOf('@') == -1 || inputEmail.value.indexOf('.' ) == -1){
-        inputEmail.style.borderColor = 'red'
+    if(inputEmail.value.indexOf('@') == -1 || 
+        inputEmail.value.indexOf('.' ) == -1 ||
+        inputEmail.value == "''" ||
+        inputEmail.value == null){
+            inputEmail.style.borderColor = 'red'
+            emailLiberado = false
     } else {
         inputEmail.style.borderColor = 'green'
+        emailLiberado = true
     }
 
 })
-
-texteareaMensagem.addEventListener('keyup', () =>{
-    if (texteareaMensagem.value.length > 100){
-        texteareaMensagem.style.borderColor = 'red'
+inputTxtArea.addEventListener('keyup', () =>{
+    if (inputTxtArea.value == '' ||
+        inputTxtArea.value == null ||
+        inputTxtArea.value.length > 50){
+            inputTxtArea.style.borderColor = 'red'
+            txtAreaLiberado = false
     } else{
-        texteareaMensagem.style.borderColor = 'green'
+        inputTxtArea.style.borderColor = 'green'
+        txtAreaLiberado = true
     }
+
+    if (nomeLiberado && emailLiberado && txtAreaLiberado){
+        btnEnviar.disabled = false
+    } else{
+        btnEnviar.disabled = true
+    }
+
 })
 
 btnEnviar.addEventListener('click', () => {
